d359d58d9ed65393f889d60996e0afa64e16ae1d
Diana Patricia Arnao Gonzalez <70480017+dpatriciaarnao@users.noreply.github.com>
2023-05-16 10:33:51 -0500

    explicacion arquitectura, patron de diseno

--- presentation/src/main/java/com/application/presentation/views/ui/ListFilmActivity.kt
+++ presentation/src/main/java/com/application/presentation/views/ui/ListFilmActivity.kt
@@ -15,6 +15,15 @@ import com.application.presentation.views.viewmodels.FilmViewState
 import com.application.presentation.views.viewmodels.FilmsViewModel
 import com.google.android.material.tabs.TabLayout
 
+//La aplicación está realizada con Clean arquitecture en capas por módulos:
+// Tenemos el módulo de app que inicializa inyección de dependencias, la app y singleton.
+// Tenemos el módulo de entities que son los data class del proyecto, las entidades con sus atributos.
+// Tenemos el módulo presentation donde se encuentran los adapters, vistas, view models, layouts, etc.
+// Tenemos el módulo repository con lo relacionado a BD, API services, DAO, Interfaces, DataSources, API responses, etc.
+// Tenemos el módulo usecases donde se encuentran los casos de uso del proyecto.
+// Todos estos módulos van interactuando entre sí, es importante asegurarse que se llamen desde los gradles de cada módulo para asegurar que se puedan comunicar entre ellos y el settings.gradle.
+// El patrón de arquitectura utilizado es MVVM (Model-View-ViewModel)
+// El patrón de diseño utilizado es Singleton.
 
 class ListFilmActivity : FilmActivity() {
 
